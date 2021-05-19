80babf2d994df01cf927ce3778397ef1e1ead2c7
Dameon Davis <dad1977ut@yahoo.com>
2021-05-18 23:32:58 -0600

    updated profolio page

---
+++ src/components/pages/profolio.js
@@ -0,0 +1,166 @@
+import React from "react";
+function Profolio() {
+  return (
+    <div class="container">
+      <div class="row p-1">
+        <div class="col-md-6 p-1">
+          <div class="card">
+            <img
+              src="assets/day-scheduler.jpg"
+              class="card-img-top img-fluid"
+              alt="a day scheduler project"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Day Scheduler</h5>
+              <p class="card-text">
+                A day scheduler app that allows you to add events and saves them
+                in your browser.
+              </p>
+              <a
+                href="https://dad1977ut.github.io/Work-Day-Scheduler/"
+                class="btn btn-primary"
+              >
+                Open website
+              </a>
+              <a
+                href="https://github.com/dad1977ut/Work-Day-Scheduler"
+                class="btn btn-primary"
+              >
+                Open repo
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 p-1">
+          <div class="card">
+            <img
+              src="assets/travel.jpg"
+              class="card-img-top img-fluid"
+              alt="Travel planner project"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Travel planner</h5>
+              <p class="card-text">This app is used to plan an upcoming trip</p>
+              <a
+                href="https://arianaw15.github.io/Trip-Itinerary/"
+                class="btn btn-primary"
+              >
+                Open website
+              </a>
+              <a
+                href="https://github.com/arianaw15/Trip-Itinerary"
+                class="btn btn-primary"
+              >
+                Open repo
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row p-1">
+        <div class="col-md-6 p-1">
+          <div class="card">
+            <img
+              src="assets/start.jpg"
+              class="card-img-top img-fluid"
+              alt="quiz"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Code Quiz</h5>
+              <p class="card-text">This a code quiz app</p>
+              <a
+                href="https://dad1977ut.github.io/Code-Quiz/"
+                class="btn btn-primary"
+              >
+                Open website
+              </a>
+              <a
+                href="https://github.com/dad1977ut/Code-Quiz"
+                class="btn btn-primary"
+              >
+                Open repo
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 p-1">
+          <div class="card">
+            <img
+              src="assets/workout.jpg"
+              class="card-img-top img-fluid"
+              alt="workout"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Workout-Tracker</h5>
+              <p class="card-text">This is a app to help track workouts</p>
+              <a
+                href="https://github.com/dad1977ut/Workout-Tracker"
+                class="btn btn-primary"
+              >
+                Open website
+              </a>
+              <a
+                href="https://obscure-chamber-77092.herokuapp.com/"
+                class="btn btn-primary"
+              >
+                Open repo
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row p-1">
+        <div class="col-md-6 p-1">
+          <div class="card">
+            <img
+              src="assets/burger_app.jpg"
+              class="card-img-top img-fluid"
+              alt="workout"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Burger app</h5>
+              <p class="card-text">Creates burgers</p>
+              <a
+                href="https://github.com/dad1977ut/burger"
+                class="btn btn-primary"
+              >
+                Open website
+              </a>
+              <a
+                href="https://lit-dawn-18330.herokuapp.com/"
+                class="btn btn-primary"
+              >
+                Open repo
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 p-1">
+          <div class="card">
+            <img
+              src="assets/end.jpg"
+              class="card-img-top img-fluid"
+              alt="workout"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Password Generator</h5>
+              <p class="card-text">Creates a password</p>
+              <a
+                href="https://github.com/dad1977ut/Password-Generator"
+                class="btn btn-primary"
+              >
+                Open website
+              </a>
+              <a
+                href="https://dad1977ut.github.io/Password-Generator/"
+                class="btn btn-primary"
+              >
+                Open repo
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
