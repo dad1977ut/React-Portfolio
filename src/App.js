f6c2d7dcd17413bbaa1f94c1d5d5d5349bedb618
Dameon Davis <dad1977ut@yahoo.com>
2021-05-21 23:29:23 -0600

    add code to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Navebar from "./components/navebar";
+import Home from "./components/pages/home";
+import Portfolio from "./components/pages/portfolio";
+import Contact from "./components/pages/contact";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navebar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
 
